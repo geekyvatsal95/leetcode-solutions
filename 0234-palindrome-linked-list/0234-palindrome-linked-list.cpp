00f4f768dde7f995e446bfc3598805a6d2c6343b
Vatsal Mishra <mishravatsal7@gmail.com>
2026-07-05 12:28:59 +0530

    Time: 4 ms (50.04%), Space: 121.8 MB (43.9%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,20 +1,39 @@
 class Solution {
 public:
+    ListNode* reverseList(ListNode* head){
+        ListNode* prev=NULL;
+        ListNode* nextNode=NULL;
+        ListNode* curr=head;
+        while(curr!=NULL){
+            nextNode=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=nextNode;
+        }
+        return prev;
+    }
     bool isPalindrome(ListNode* head) {
-        stack<int> st;
-        ListNode* temp=head;
-        while(temp!=NULL){
-            st.push(temp->val);
-            temp=temp->next;
+        //find left middle
+        ListNode* slow=head;
+        ListNode* fast=head->next;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
         }
-        temp=head;
-        while(temp!=NULL){
-            if(temp->val!=st.top()) return false;
-            st.pop();
-            temp=temp->next;
+        ListNode* head1=reverseList(slow->next);
+        ListNode* temp1=head;
+        ListNode* temp2=head1;
+        while(temp2!=NULL){
+            if(temp1->val!=temp2->val) {
+                reverseList(head1);
+                return false;
+            }
+            temp1=temp1->next;
+            temp2=temp2->next;
         }
+        reverseList (head1);
         return true;
-        
+
 
 
     }
