86e1af9af9b58bdff1c52522a321a359b89cabaf
Vatsal Mishra <mishravatsal7@gmail.com>
2026-07-09 10:13:01 +0530

    Time: 16 ms (7.5%), Space: 14.6 MB (9.38%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        unordered_set<ListNode*> st;
+        ListNode* temp=head;
+        while(temp!=NULL){
+            if(st.find(temp)!=st.end()){
+                return true;
+            }
+            st.insert(temp);
+            temp= temp->next;
+        }
+        return false;
+        
+
+    }
+};
