8490f920e3b49cafccb49d23e4f02ccb4c9acc57
Vatsal Mishra <mishravatsal7@gmail.com>
2026-07-23 18:50:39 +0530

    Time: 0 ms (100%), Space: 23.9 MB (19.51%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        vector<int> temp;
         int n=nums.size();
+        //find first 0 position;
+        int j=-1;
         for(int i=0;i<n;i++){
-            if(nums[i]!=0) temp.push_back(nums[i]);
+            if(nums[i]==0){
+                j=i;
+                break;
+            }
         }
-        for(int i=0;i<temp.size();i++){
-            nums[i]=temp[i];
+        if(j==-1) return;
+        for(int i=j+1;i<n;i++){
+            if(nums[i]!=0) {
+                swap(nums[i],nums[j]);
+                j++;
+            }
         }
-        for(int i=temp.size();i<n;i++){
-            nums[i]=0;
-        }
+
         
     }
 };
