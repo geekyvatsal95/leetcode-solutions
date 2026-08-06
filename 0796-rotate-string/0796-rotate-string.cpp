b6050050e5c47156aa7705dab9a3a03cfca8b381
Vatsal Mishra <mishravatsal7@gmail.com>
2026-08-06 16:07:39 +0530

    Time: 0 ms (100%), Space: 8.2 MB (35.77%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        int n = s.length();
-        if (n != goal.length()) return false;
-
-        int dfa[101][26] = {0};
-        dfa[0][s[0] - 'a'] = 1;
-
-        int x = 0;
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < 26; j++)
-                dfa[i][j] = dfa[x][j];
-            int c = s[i] - 'a';
-            dfa[i][c] = i + 1;
-            x = dfa[x][c];
+        int n1=s.size();
+        int n2=goal.size();
+        if(n1!=n2) return false;
+        string left="";
+        for(int i=0;i<n1;i++){
+            string right= s.substr(i);
+            if(right+left==goal) return true;
+            left+=s[i];
         }
-
-        int state = 0;
-        for (int i = 0; i < n << 1; i++) {
-            state = dfa[state][goal[i % n] - 'a'];
-            if (state == n)
-                return true;
-        }
-
         return false;
+        
     }
 };
