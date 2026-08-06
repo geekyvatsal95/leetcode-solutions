139a23d0c7b6a7fe6a0b8c89d24c03c71c234e26
Vatsal Mishra <mishravatsal7@gmail.com>
2026-08-06 16:24:34 +0530

    Time: 0 ms (100%), Space: 9.5 MB (99.43%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,10 +2,15 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size()) return false;
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t) return true;
-        return false;
-        
+        int freq_s[26];
+        int freq_t[26];
+        for(int i=0;i<s.size();i++){
+            freq_s[s[i]-'a'] ++;
+            freq_t[t[i]-'a'] ++;
+        }
+        for(int i=0;i<26;i++){
+            if(freq_s[i]!=freq_t[i]) return false; 
+        }
+        return true;
     }
 };
