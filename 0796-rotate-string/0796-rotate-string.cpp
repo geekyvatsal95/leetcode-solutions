7bc5130fce1c6d0c10c428120832c34a81df9bff
Vatsal Mishra <mishravatsal7@gmail.com>
2026-08-06 16:13:42 +0530

    Time: 0 ms (100%), Space: 7.8 MB (98.16%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -4,13 +4,11 @@ public:
         int n1=s.size();
         int n2=goal.size();
         if(n1!=n2) return false;
-        string left="";
-        for(int i=0;i<n1;i++){
-            string right= s.substr(i);
-            if(right+left==goal) return true;
-            left+=s[i];
-        }
-        return false;
+        string doubled = s+s;
+        if(doubled.find(goal)!=string :: npos) return true;
+        else return false;
+        
+    
         
     }
 };
