829543a005f23096039a77c3e62ae9fab97b80cf
Vatsal Mishra <mishravatsal7@gmail.com>
2026-08-11 16:35:00 +0530

    Time: 0 ms (100%), Space: 12.4 MB (6.99%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        map<int,int> mpp; // key : number , value :  occurence
+        vector<int> ans (2001);
         for(int i=0;i<arr.size();i++){
-            mpp[arr[i]]++;
+            ans[arr[i]+1000]++;
         }
-        unordered_set<int> st;
-        for(auto it : mpp){
-            if(st.find(it.second)!=st.end()) return false;
-            st.insert(it.second);
+        sort(ans.begin(),ans.end());
+        for(int i=0;i<2000;i++){
+            if(ans[i]!=0 && ans[i]==ans[i+1]) return false;
         }
         return true;
+
+
+
+
+
+
+
+
+
+
+
+
+        // map<int,int> mpp; // key : number , value :  occurence
+        // for(int i=0;i<arr.size();i++){
+        //     mpp[arr[i]]++;
+        // }
+        // unordered_set<int> st;
+        // for(auto it : mpp){
+        //     if(st.find(it.second)!=st.end()) return false;
+        //     st.insert(it.second);
+        // }
+        // return true;
 
         
     }
