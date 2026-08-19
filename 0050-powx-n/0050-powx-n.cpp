ed7e174a8f4e73495b6b46155c98a4aa7a9994c8
Vatsal Mishra <mishravatsal7@gmail.com>
2026-08-19 16:41:56 +0530

    Time: 0 ms (100%), Space: 8.6 MB (71.57%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     double myPow(double x, long long n) {
+        if(n==0) return 1;
         if(n<0){
             x=1/x;
-            n=-1*n;
+            n=-1*(n);
         }
-        if(n==0) return 1;
-        if(n%2==0) return myPow(x*x,n/2) ;
-        else return (x*myPow(x,n-1));
-
-
+        double ans=1;
+        while(n>0){
+            if(n%2==0){
+                n/=2;
+                x*=x;
+            }
+            else{
+                n-=1;
+                ans*=x;
+            }
+        }
+        return ans;
+        
     }
 };
