fb7c24af328dea17bf972634c6a3bdcf514699c6
Vatsal Mishra <mishravatsal7@gmail.com>
2026-09-02 19:49:28 +0530

    Time: 3 ms (82.56%), Space: 14.4 MB (34.88%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    void reverseStack(stack<char>& st , string& s){
-        if(st.size()==0) return;
-        char ch = st.top();
-        st.pop();
-        reverseStack(st,s);
-        s+=ch;
-        st.push(ch);
-    }
+
     string removeDuplicates(string s) {
-        int n=s.size();
+        int n= s.size();
         stack<char> st;
-        for(auto it : s){
-            if(st.empty()==true) st.push(it);
-            else{
-                if(it==st.top()) st.pop();
-                else st.push(it);
-            }
+        for(auto it: s){
+            if(st.size()>0 && it==st.top()) st.pop();
+            else st.push(it);
         }
-        string ans="";
-        reverseStack(st,ans);
-        return ans;
+        string str="";
+        while(!st.empty()){
+            str+=st.top();
+            st.pop();
+        }
+        reverse(str.begin(),str.end());
+        return str;
+
+
     }
 };
